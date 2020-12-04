86ac672d0fec932de7ad23c748bbb3df20f56db6
Javier <javier.fuentesm@hotmail.com>
2020-12-04 06:51:15 -0500

    Code Split excercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,11 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which using a dynamic import
 // to get the Globe component from the '../globe' module.
+const Globe = React.lazy(() => import('../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -35,7 +36,11 @@ function App() {
         {' show globe'}
       </label>
       <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+        {showGlobe ? (
+          <React.Suspense fallback={<div>loading...</div>}>
+            <Globe />
+          </React.Suspense>
+        ) : null}
       </div>
     </div>
   )
